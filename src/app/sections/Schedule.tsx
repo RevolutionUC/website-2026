f54180a43b4b63b15d3db1fcdbe6369f99b7bc2b
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-15 17:15:34 -0500

    schedule section under construction

--- src/app/sections/Schedule.tsx
+++ src/app/sections/Schedule.tsx
@@ -1,22 +1,23 @@
-import SplitText from "@/app/effects/SplitText";
+// import SplitText from "@/app/effects/SplitText";
+import Image from "next/image";
 export default function Schedule() {
   return (
     <div id="schedule" className="section w-full h-screen relative overflow-hidden">
       {/* Content */}
-      <div className="relative z-20 w-full h-full flex items-start justify-end pt-[10%] px-4 sm:px-6 lg:px-8">
-        <div className="max-w-2xl mr-[10%]">
-          <SplitText
-            text="Schedule"
-            className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight"
-            delay={50}
-            duration={0.6}
-            ease="power3.out"
-            splitType="chars"
-            from={{ opacity: 0, y: 40 }}
-            to={{ opacity: 1, y: 0 }}
-            threshold={0.1}
-            rootMargin="-100px"
-            textAlign="left"
+      <div className="relative z-20 w-full h-full flex items-center justify-center pt-[10%] px-4 sm:px-6 lg:px-8">
+        <div className="font-mono text-4xl flex flex-col items-center">
+          This section is under construction.
+          <br />
+          <span className="underline underline-offset-4 font-semibold decoration-[#19E363] ">
+            Mr. Panda is very busy right now!
+          </span>
+          <Image
+            src="/panda_construction.webp"
+            alt="Panda under construction"
+            width={400}
+            height={400}
+            style={{ height: "auto" }}
+            className="hover:scale-105 hover:rotate-12 transition-all hover:cursor-pointer"
           />
         </div>
       </div>
