6b3752c48aa26296bfe4fc53954574f8b9f82307
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-16 14:05:19 -0500

    update: prizes -> tracks [skip deploy]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/app/sections/Hero";
 import About from "@/app/sections/About";
 import Clouds from "@/app/sections/Clouds";
 import BoardingPass from "@/app/sections/BoardingPass";
-import Prizes from "@/app/sections/Prizes";
+import Prizes from "@/app/sections/Tracks";
 import Stats from "@/app/sections/Stats";
 import Faq from "@/app/sections/FAQ";
 import Schedule from "@/app/sections/Schedule";
