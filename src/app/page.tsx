559c93e89a63ffbe2aa321e8c574a416027967cf
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-22 22:39:26 +0530

    clearing git cache and re-add files [skip deploy]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/app/sections/Hero";
 import About from "@/app/sections/About";
 import Clouds from "@/app/sections/Clouds";
 import BoardingPass from "@/app/sections/BoardingPass";
+import Sponsors from "@/app/sections/Sponsors"
 import Prizes from "@/app/sections/Tracks";
 import Stats from "@/app/sections/Stats";
 import Faq from "@/app/sections/FAQ";
@@ -17,7 +18,7 @@ const Home = () => {
       <About />
       <BoardingPass />
       <Stats />
-      {/*<Sponsors />*/}
+      <Sponsors />
       <Prizes />
       <Faq />
       <Schedule />
