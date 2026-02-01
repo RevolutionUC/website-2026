a40ce211dafa458610c8afbbda62f3d0745c9690
Tam Le <lengoctam0106@gmail.com>
2026-02-01 14:51:18 -0500

    updated MLH badge [skip deploy]

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -102,25 +102,15 @@ export function NavigationBar() {
       {/* MLH Trust Badge */}
       <a
         id="mlh-trust-badge"
-        style={{
-          display: "block",
-          maxWidth: "100px",
-          minWidth: "60px",
-          position: "fixed",
-          right: "50px",
-          top: 0,
-          width: "10%",
-          zIndex: 10000,
-          marginLeft: "20%",
-        }}
-        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=blue"
+        className="block fixed top-0 right-4 sm:right-8 lg:right-[50px] w-[10%] max-w-[100px] min-w-[60px] z-[10000]"
+        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
         target="_blank"
         rel="noopener noreferrer"
       >
         <img
-          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-blue.svg"
+          src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
           alt="Major League Hacking 2026 Hackathon Season"
-          style={{ width: "100%" }}
+          className="w-full"
         />
       </a>
 
